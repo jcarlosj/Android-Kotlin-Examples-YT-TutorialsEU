743fe25a997efb7560f9ba6dc4aa30734067b2d9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-17 17:59:15 -0500

    [+] Model: User Class

---
+++ app/src/main/java/com/github/tutorialseu/models/User.kt
@@ -0,0 +1,13 @@
+package com.github.tutorialseu.models
+
+// Clase modelo con estructura de datos de usuario
+class User (
+    val id: Int,
+    val name: String,
+    val email: String,
+    val gender: String,
+    val weight: Double,
+    val height: Int,
+    val mobile: String,
+    val office: String
+)
