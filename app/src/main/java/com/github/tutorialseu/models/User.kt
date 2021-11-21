d83715309a62344274a02482f6966ff11c1d1243
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-21 09:57:38 -0500

    [/] Model: UserList, User, Phone Classes

--- app/src/main/java/com/github/tutorialseu/models/User.kt
+++ app/src/main/java/com/github/tutorialseu/models/User.kt
@@ -1,13 +1,21 @@
 package com.github.tutorialseu.models
 
+data class UserList (
+    val users: ArrayList<User>
+)
+
 // Clase modelo con estructura de datos de usuario
-class User (
+data class User (
     val id: Int,
     val name: String,
     val email: String,
     val gender: String,
     val weight: Double,
     val height: Int,
+    val phone: Phone
+)
+
+data class Phone (
     val mobile: String,
     val office: String
 )
